303e82b6aaf37841e70ba7ee2684691ec88b46a1
WooneoSeop <128963947+WooneoSeop@users.noreply.github.com>
2023-03-29 02:05:12 +0900

    Add files via upload

---
+++ 동아리_과제01.py
@@ -0,0 +1,67 @@
+#1
+station = "사당" # "신도림", "인천공항"
+print(station)
+
+#2
+from random import *
+a = randint(4,28)
+print("오프라인 스터디 모음 날짜는 매월 ", str(a)+"일로 선정되엇습니다.")
+
+#3
+print("Hello")
+
+#4
+b = "Mary's cosmetics"
+print(b)
+
+#5
+c = '"도둑이야".'
+print("신씨가 소리질렀다.",c)
+
+#6
+# 예상 : 8
+
+#7
+# 예상 : 34
+
+#8
+print("#"*100)
+
+#9
+print("평균 매입 금액 : ", str(87720/34)+"원")
+
+#10
+letters = "python"
+print("첫 번째 : ", letters[0])
+print("세 번째: ", letters[2])
+
+#11
+license_plate = "24가 2210"
+print("뒤 네 자리 : ", license_plate[4:])
+
+#12
+string = "홀짝홀짝홀짝"
+print("홀만 추출 : ", string[0::2])
+
+#13
+string = "PYTHON"
+print("문자열을 거꾸로 뒤집어서 출력 : ", string[::-1])
+
+#14
+phone_number = "010-1111-2222"
+print("하이푼 제거하고 추출 : ", phone_number.replace("-"," "))
+
+#15
+phone_number = "010-1111-2222"
+print("하이푼 제거하고 추출 : ", phone_number.replace("-",""))
+
+#16
+url = "http://sharebook.kr"
+print("도메인만 출력 :", url[17:19])
+
+#17
+string = 'abcdfe2a354a32a'
+print(string.replace("a","A"))
+
+#18
+# 예상 : abcd
